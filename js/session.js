505d85dca32c3b38a16cabd07eb2641b04e96e1e
Nikhil Singh <nikhilsingh498@gmail.com>
2020-04-20 04:16:44 +0530

    Token issue fix

--- js/session.js
+++ js/session.js
@@ -1,26 +1,21 @@
 function getToken(currToken) {
 	console.log(currToken)
-	var networkPromise = fetch('https://thecodeclutch.herokuapp.com/auth/getToken', {
+	var dataPromise = fetch('https://thecodeclutch.herokuapp.com/auth/getToken', {
 		method: 'GET',
 		headers: {
 			Authorization: currToken
 		}
 	});
-	var timeOutPromise = new Promise(function (resolve, reject) {
-			setTimeout(resolve, 20000);
-	});
-
-	Promise.all(
-		[networkPromise, timeOutPromise]).then(function (values) {
-			console.log("Atleast 2 secs + TTL (Network/server)");
-			values[0].then(data => {
-				return data.json();
-			})
-			.then( data => {
-				console.log(data)
-				localStorage.setItem('TCC_SID', data.token)
-				getToken(localStorage.getItem('TCC_SID'));
-			})
-		});
+	dataPromise.then(data => {
+		return data.json()
+	})
+	.then( val => {
+		localStorage.setItem('TCC_SID', data.token)
+	})
+	.catch( err => {
+		console.log(err)
+	})
 }
-getToken(localStorage.getItem('TCC_SID'))
+window.setInterval(function(){
+  getToken(localStorage.getItem('TCC_SID'))
+}, 10000);
