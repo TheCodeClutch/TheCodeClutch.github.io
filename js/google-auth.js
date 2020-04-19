65089cef1a2c4187dc2e2cb84249ebe78edfa18b
Nikhil Singh <nikhilsingh498@gmail.com>
2020-04-20 03:35:41 +0530

    Add getToken()

--- js/google-auth.js
+++ js/google-auth.js
@@ -1,9 +1,29 @@
 fetch('https://thecodeclutch.herokuapp.com/auth/getGoogleAuthUrl')
-.then( res => {
-	return res.json();
-})
-.then( data => {
-	document.getElementById('google-login').setAttribute('href', data.url);
-})
-.catch( err => console.log(err))
+	.then(res => {
+		return res.json();
+	})
+	.then(data => {
+		document.getElementById('google-login').setAttribute('href', data.url);
+	})
+	.catch(err => console.log(err))
 
+
+function getToken(currToken) {
+	var networkPromise = fetch('https://thecodeclutch.herokuapp.com/auth/getToken', {
+		method: 'GET',
+		headers: {
+			Authorization: currToken
+		}
+	});
+	var timeOutPromise = new Promise(function (resolve, reject) {
+			setTimeout(resolve, 20000);
+	});
+
+	Promise.all(
+		[networkPromise, timeOutPromise]).then(function (values) {
+			console.log("Atleast 2 secs + TTL (Network/server)");
+			getToken(localStorage.setItem('TCC_SID', values.token));
+		});
+}
+
+getToken(localStorage.getItem('TCC_SID'))
