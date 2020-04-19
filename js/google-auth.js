ff05e31ed5f87896dfdcbcfa5ee7dfcb25fcf878
Nikhil Singh <nikhilsingh498@gmail.com>
2020-04-19 06:04:01 +0530

    Add google login utl

---
+++ js/google-auth.js
@@ -0,0 +1,9 @@
+fetch('https://thecodeclutch.herokuapp.com/auth/getGoogleAuthUrl')
+.then( res => {
+	return res.json();
+})
+.then( data => {
+	document.getElementById('google-login').setAttribute('href', data.url);
+})
+.catch( err => console.log(err))
+
